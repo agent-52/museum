71ba28e156550c80d231a0123a599bb987a0df4c
agent-52 <abhaybhadauriya52@gmail.com>
2024-08-21 14:37:14 +0530

    complete page 1

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from '../assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,22 +6,58 @@ import Header from '../Header/Header'
 import Button from '../Button/Button'
 import searchIcon from "/Images/searchIcon.png"
 
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { TextPlugin } from "gsap/TextPlugin";
+
+gsap.registerPlugin(useGSAP);
+gsap.registerPlugin(TextPlugin)
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  
+  useGSAP(()=>{
+    const tl = gsap.timeline()
+    tl.to("span.india", {
+      text: "BHARAT",
+      duration:1
+    })
+    tl.to("span.india", {
+      text: "भारत",
+      duration:1
+    })
+    tl.to("span.india", {
+      text: "ਭਾਰਤ",
+      duration:1
+    })
+    tl.to("span.india", {
+      text: "ಭಾರತ",
+      duration:1
+    })
+    tl.to("span.india", {
+      text: "INDIAN",
+      duration:1
+    })
+  })
 
   return (
-    <>
+    <div className='body flexC justifyC border '>
       <Header />
-      <div className="dec1Button">Online Tickets</div>
-      <h1>Explore the Great <span>INDIAN</span> History</h1> 
-      <h2>Buy <span>Tickets</span> of museums <span>Online</span></h2>
-      <div>invest your time exploring rather than waiting in a queue</div>
+      <div className='flexC justifyC alignC gap1 h100'>
+        <div className="dec1ButtonWrapper"><div className='bgW'><div className='dec1Button'>Online Museum Tickets</div></div></div>
+        <div className='mgB1'>
+          <h1 className='fsXl lightBold'>Explore the great</h1>
+          <h1 className='fsXl lightBold'><span className='india' id='india'></span> History</h1> 
+        </div>
+        <div className='fs1 maxW40Ch'>Buy <span className='bold'>Tickets</span> of museums <span className='bold'>Online</span> and invest your time exploring rather than waiting in a queue</div>
+      
 
-      <div>
-        <input type="text" className='serachBox' placeholder='Search Museum name'/>
-        <button className='searchButton'><img src={searchIcon} alt="" className='boxImage'/></button>
+        <div className='flex gap00 justifyC wMax'>
+          <input type="text" className='searchBox whiteHover textC' placeholder='Search Museum name'/>
+          <button className='searchButton'><img src={searchIcon} alt="" className='boxImage'/></button>
+        </div>
       </div>
-    </>
+    </div>
   )
 }
 
--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -4,14 +4,14 @@ import Button from "../Button/Button.jsx"
 const Header = () =>{
 
   return(
-    <header>
-      <div>
-        <div></div>
-        <div></div>
-        <div></div>
+    <header className="flex pdI2 justifyBw w100 pdB0 alignC ">
+      <div className="flex gap1 alignC">
+        <div className="lightBold cLw">museums</div>
+        <div className="lightBold cLw">tickets</div>
+        <div className="lightBold cLw"></div>
         <Button text="Register Museum"/>
       </div>
-      <div>
+      <div className="flex gap1">
         
         <Button text="User login" classArray="blackHover"/>
         <Button text="Admin login" classArray="bgW1 cB1 whiteHover"/>
