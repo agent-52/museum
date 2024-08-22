296f131bb127c2ca68ced49d053cc8a8f7b56193
agent-52 <abhaybhadauriya52@gmail.com>
2024-08-22 14:25:09 +0530

    add three js structure and set up museumhome page

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App/App.jsx'
 import './index.css'
+import MuseumHomePage from './MuseumHomePage/MuseumHomePage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <MuseumHomePage />
   </StrictMode>,
 )
