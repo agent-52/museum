8ed4934b3024d6fb5fae546b4f1a2f1c58ae0e12
agent-52 <abhaybhadauriya52@gmail.com>
2024-08-30 06:44:02 +0530

    test connection by making another server

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const cors = require("cors")
+const app = express()
+
+app.use(express.urlencoded({extended: true}))
+app.use(cors())
+
+let data = {
+  "name": ["abhay", "anshul"]
+};
+
+app.get("/api", (req, res) =>{
+  
+  res.json(data)
+})
+
+app.post("/api", async (req, res) =>{
+  
+  data = await req.body;
+  res.redirect("/")
+})
+
+const PORT =  5000
+app.listen(PORT, () =>{
+  console.log(`server up and running on port${PORT}`)
+  console.log(data)
+})
--- src/chatBotConnect/ChatBotConnect.jsx
+++ src/chatBotConnect/ChatBotConnect.jsx
@@ -2,59 +2,23 @@ import { useState } from "react";
 import { useEffect } from "react";
 
 const ChatBotConnect = () =>{
-
-
-  
   const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true);
-
+ 
   useEffect(() => {
-
-    fetch("https://server1-production-9559.up.railway.app/", {mode: "no-cors"})
-     .then((response) => {
-        if (response.status >= 400) {
-          throw new Error("server error");
-        }
-        console.log(response)
-        return response.json();
-      })
-      .then((response) =>{
-        console.log(response)
-        return setData(response)
-      } )
-      
-      .catch((error) => {
-        console.log(error)
-        return setError(error)}
-        )
-      .finally(() => setLoading(false));
     
-    // fetch("https://server1-production-9559.up.railway.app/", {
-    //    mode: "cors",
-    //    method: 'post',
-    //    headers: {'Content-Type':'application/json'},  
-    //     body: JSON.stringify({
-    //      "firstParam": 'yourValue',
-    //       "secondParam": 'yourOtherValue',
-    //     })
-    //   })
-    // .then((response) => {
-    //   if (response.status >= 400) {
-    //     throw new Error("server error");
-    //   }
-    //   return response.json();
-    // })
-    // .then((response) => setData(response))
-    // .catch((error) => setError(error))
-    // .finally(() => setLoading(false));
+    fetch("http://localhost:5000/api", {method:"GET"}).then(
+      response => response.json()
+    ).then(
+      newdata => {
+        console.log(newdata)
+        setData(newdata)}
+    ).catch(error => console.log(error))
+    
+    
   }, []);
   
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>A network error was encountered</p>;
-
   return(
-    <div className="clB">{data}</div>
+    <div></div>
   )
 }
 
