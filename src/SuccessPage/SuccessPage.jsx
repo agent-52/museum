7bf35224ffd1e75d6632030ff50cbbf2a0a35502
agent-52 <abhaybhadauriya52@gmail.com>
2024-08-31 13:41:27 +0530

    make email link

--- src/SuccessPage/SuccessPage.jsx
+++ src/SuccessPage/SuccessPage.jsx
@@ -9,7 +9,7 @@ const SuccessPage = () =>{
     async function getEmail(){
       
      await fetch("http://localhost:8081/api/visit/send-email").catch((error) => console.log(error))
-     console.log("emailSent")
+     document.querySelector(".emailText").textContent = `Ticket details sent at your registered ${<a href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox">"email"</a>}`
       
     }
     getEmail()
@@ -20,8 +20,8 @@ const SuccessPage = () =>{
     <div className="h100 successPage clB">
       <div className="successIcon"><img src={successIcon} className="boxImage" alt="" /></div>
       <div className="fsXl lightBold">Success</div>
-      <div className="fs2">Payment Completed Successfully</div>
-      <div>Ticket details sent at your registered email</div>
+      <div className="fs2 emailText">Payment Completed Successfully</div>
+      <div></div>
 
     </div>
   )
